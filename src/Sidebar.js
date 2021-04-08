8aef8dac2d8a190ba7b02571d4a49d61a3dec141
Deep Kiran <cdeepkiran14@gmail.com>
2021-04-08 12:16:47 +0530

    Refactor Code

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,9 +2,5 @@ import React from "react";
 import "./ProductView.css";
 
 export default function Sidebar() {
-  return (
-    <div className="sidebar">
-      <p>Filters</p>
-    </div>
-  );
+  return <div className="sidebar">Filters</div>;
 }
